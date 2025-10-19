fd61c4cdb0167cb912e17c46b14f064a007e779b
sungwoo-yang <sungwoo.yang@digipen.edu>
2025-10-20 05:33:02 +0900

    123

--- Assets/shaders/ImmediateRenderer2D/quad.vert
+++ Assets/shaders/ImmediateRenderer2D/quad.vert
@@ -8,18 +8,31 @@
  * \copyright DigiPen Institute of Technology
  */
 
-layout (location = 0) in vec2 a_position;
-layout (location = 1) in vec2 a_tex_coord;
+layout (location = 0) in vec2 a_position; // Vertex position (local space, typically -0.5 to 0.5)
+layout (location = 1) in vec2 a_tex_coord; // Texture coordinate (typically 0.0 to 1.0)
 
-uniform mat3 u_ndc_matrix;
-uniform mat3 u_model_matrix;
-uniform mat3 u_uv_matrix;
+// Uniforms provided by the C++ application
+uniform mat3 u_ndc_matrix;   // Matrix to convert from world space to Normalized Device Coordinates (NDC)
+uniform mat3 u_model_matrix; // Matrix to transform the quad from local space to world space (includes translation, rotation, scale)
+uniform mat3 u_uv_matrix;    // Matrix to transform the input texture coordinates (for sprite sheets/atlases)
 
-out vec2 v_uv;
+out vec2 v_uv; // Pass the transformed texture coordinate to the fragment shader
 
 void main()
 {
+    // 1. Transform the input texture coordinate using the uv_matrix.
+    //    We use a vec3 because matrix multiplication requires matching dimensions.
+    //    The .xy extracts the resulting 2D coordinate.
     v_uv = (u_uv_matrix * vec3(a_tex_coord, 1.0)).xy;
+
+    // 2. Transform the vertex position:
+    //    a) Convert local position (a_position) to a vec3 (adding z=1.0 for matrix multiplication).
+    //    b) Apply the model matrix to transform from local space to world space.
+    //    c) Apply the NDC matrix to transform from world space to NDC space.
     vec3 ndc_pos = u_ndc_matrix * u_model_matrix * vec3(a_position, 1.0);
+
+    // 3. Set the final vertex position for OpenGL.
+    //    gl_Position requires a vec4. We use the calculated ndc_pos.xy for x and y,
+    //    0.0 for z (since we're in 2D), and 1.0 for w (standard for positions).
     gl_Position = vec4(ndc_pos.xy, 0.0, 1.0);
 }
--- source/Engine/Matrix.cpp
+++ source/Engine/Matrix.cpp
@@ -8,52 +8,96 @@
  * \copyright DigiPen Institute of Technology
  */
 
-#pragma once
-#include "Vec2.hpp"
+#include "Matrix.hpp"
+#include <cmath>
 
 namespace Math
 {
-    class TransformationMatrix
+    TransformationMatrix::TransformationMatrix()
     {
-    public:
-        TransformationMatrix();
+        Reset();
+    }
 
-        constexpr double* operator[](int index)
+    void TransformationMatrix::Reset()
+    {
+        for (int i = 0; i < 3; ++i)
         {
-            return matrix[index];
+            for (int j = 0; j < 3; ++j)
+            {
+                matrix[i][j] = 0.0;
+            }
         }
 
-        constexpr const double* operator[](int index) const
-        {
-            return matrix[index];
-        }
+        matrix[0][0] = 1.0;
+        matrix[1][1] = 1.0;
+        matrix[2][2] = 1.0;
+    }
 
-        TransformationMatrix  operator*(TransformationMatrix m) const;
-        TransformationMatrix& operator*=(TransformationMatrix m);
-        vec2                  operator*(vec2 v) const;
-        void                  Reset();
-
-    protected:
-        double matrix[3][3];
-    };
-
-    class TranslationMatrix : public TransformationMatrix
+    TransformationMatrix TransformationMatrix::operator*(TransformationMatrix m) const
     {
-    public:
-        TranslationMatrix(ivec2 translate);
-        TranslationMatrix(vec2 translate);
-    };
+        TransformationMatrix result;
 
-    class RotationMatrix : public TransformationMatrix
-    {
-    public:
-        RotationMatrix(double theta);
-    };
+        result.matrix[0][0] = matrix[0][0] * m[0][0] + matrix[0][1] * m[1][0] + matrix[0][2] * m[2][0];
+        result.matrix[0][1] = matrix[0][0] * m[0][1] + matrix[0][1] * m[1][1] + matrix[0][2] * m[2][1];
+        result.matrix[0][2] = matrix[0][0] * m[0][2] + matrix[0][1] * m[1][2] + matrix[0][2] * m[2][2];
+        result.matrix[1][0] = matrix[1][0] * m[0][0] + matrix[1][1] * m[1][0] + matrix[1][2] * m[2][0];
+        result.matrix[1][1] = matrix[1][0] * m[0][1] + matrix[1][1] * m[1][1] + matrix[1][2] * m[2][1];
+        result.matrix[1][2] = matrix[1][0] * m[0][2] + matrix[1][1] * m[1][2] + matrix[1][2] * m[2][2];
+        result.matrix[2][0] = matrix[2][0] * m[0][0] + matrix[2][1] * m[1][0] + matrix[2][2] * m[2][0];
+        result.matrix[2][1] = matrix[2][0] * m[0][1] + matrix[2][1] * m[1][1] + matrix[2][2] * m[2][1];
+        result.matrix[2][2] = matrix[2][0] * m[0][2] + matrix[2][1] * m[1][2] + matrix[2][2] * m[2][2];
 
-    class ScaleMatrix : public TransformationMatrix
+        return result;
+    }
+
+    TransformationMatrix& TransformationMatrix::operator*=(Math::TransformationMatrix m)
     {
-    public:
-        ScaleMatrix(double scale);
-        ScaleMatrix(vec2 scale);
-    };
+        (*this) = (*this) * m;
+        return (*this);
+    }
+
+    vec2 TransformationMatrix::operator*(vec2 v) const
+    {
+        vec2 result;
+        result.x = matrix[0][0] * v.x + matrix[0][1] * v.y + matrix[0][2];
+        result.y = matrix[1][0] * v.x + matrix[1][1] * v.y + matrix[1][2];
+        return result;
+    }
+
+    TranslationMatrix::TranslationMatrix(ivec2 translate)
+    {
+        Reset();
+        matrix[0][2] = static_cast<double>(translate.x);
+        matrix[1][2] = static_cast<double>(translate.y);
+    }
+
+    TranslationMatrix::TranslationMatrix(vec2 translate)
+    {
+        Reset();
+        matrix[0][2] = translate.x;
+        matrix[1][2] = translate.y;
+    }
+
+    ScaleMatrix::ScaleMatrix(double scale)
+    {
+        Reset();
+        matrix[0][0] = scale;
+        matrix[1][1] = scale;
+    }
+
+    ScaleMatrix::ScaleMatrix(vec2 scale)
+    {
+        Reset();
+        matrix[0][0] = scale.x;
+        matrix[1][1] = scale.y;
+    }
+
+    RotationMatrix::RotationMatrix(double theta)
+    {
+        Reset();
+        matrix[0][0] = cos(theta);
+        matrix[0][1] = -sin(theta);
+        matrix[1][0] = sin(theta);
+        matrix[1][1] = cos(theta);
+    }
 }
